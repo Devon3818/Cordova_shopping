7b8ea0af5f3e6d31395aac4dedd70afd68f03d18
Devon <k849996781@vip.qq.com>
2016-07-09 17:38:25 +0800

    change
    ok

---
+++ cordova/www/js/setting.js
@@ -0,0 +1,29 @@
+myApp.onPageInit('setting', function (page) {
+	
+	$$(".toolbar").addClass('hide');
+	
+});
+
+
+myApp.onPageAfterAnimation('setting', function (page) {
+	
+	$$('.confirm-ok').on('click', function () {
+	    myApp.confirm('确定要退出?','提示', function () {
+	        myApp.alert('You clicked Ok button');
+	    });
+	});
+
+	
+});
+
+
+
+
+
+
+myApp.onPageAfterBack('setting', function (page) {
+	
+	$$(".toolbar").removeClass('hide');
+});
+
+
